938cecc3d9325b52c2325e24a0f019ff96ee0449
minhajmia <minhajmia418@gmail.com>
2022-10-25 23:11:22 +0600

    details page completed

--- src/Pages/CourseDetails/CourseDetails.js
+++ src/Pages/CourseDetails/CourseDetails.js
@@ -16,8 +16,8 @@ const CourseDetails = () => {
   } = course;
   console.log(course);
   return (
-    <div className="md:flex justify-around mt-10 text-center px-5">
-      <div className="course-info md:text-left">
+    <div className="md:flex justify-between mt-10 text-center px-5">
+      <div className="course-info md:text-left mx-5">
         <h3 className="text-2xl font-semibold">{name}</h3>
         <p>{details}</p>
         <p className="font-semibold mt-2">Requirement</p>
@@ -36,39 +36,54 @@ const CourseDetails = () => {
           <p className className="font-semibold mt-2">
             Instructor
           </p>
-          <div className="flex">
+          <div className="md:flex items-center gap-5">
             {" "}
-            <div className="flex items-center gap-5 mb-3 justify-center md:justify-start">
+            <div className=" mb-3 justify-center md:justify-start">
               <div className="avatar mt-5">
                 <div className="w-20 rounded-full ring ring-slate-400 ring-offset-base-400 ring-offset-2">
                   <img src={instructor?.photo} />
                 </div>
               </div>
-              <div>
-                {" "}
-                <p>{instructor?.name}</p>
-                <p>{instructor?.publish_date}</p>
-              </div>
-              <div>
-                <p>Total Views : {total_views}</p>
-              </div>
-              <div>
-                <p>Ratings : {rating}</p>
-              </div>
-              <div>
-                <p>Price : {price}</p>
-              </div>
+            </div>
+            <div>
+              <p>{instructor?.name}</p>
+              <p>{instructor?.publish_date}</p>
             </div>
           </div>
         </div>
       </div>
-      <div className="course-image  ">
-        <img
-          className="rounded-md inline-block px-5"
-          style={{ maxHeight: "500px" }}
-          src={picture}
-          alt=""
-        />
+      <div className="course mx-5 mt-10 md:mt-0">
+        <div className="card bg-slate-50 shadow-xl">
+          <figure>
+            <img
+              src={picture}
+              className="rounded-md inline-block px-5"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              Total Views
+              <div className="badge bg-slate-400">{total_views}</div>
+            </h2>
+            <p className="text-left">
+              <span className="font-semibold">Rating : </span>
+              {rating}
+            </p>
+            <p className="text-left">
+              <span className="font-semibold">Price : </span>${price}
+            </p>
+            <div className="card-actions justify-end">
+              <div className="badge bg-slate-400  ">
+                <Link to="/checkout">Get to Premium</Link>{" "}
+              </div>
+              <div className="badge badge-outline">
+                {" "}
+                <Link>Download</Link>{" "}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
